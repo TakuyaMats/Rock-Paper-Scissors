c0c932739ce6977ac1a29b0cd3167965b0f00450
thebadams <bpeteradams@gmail.com>
2021-02-20 12:47:43 -0500

    added array options [R, P S]

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // define Rock, Paper, Scissors as viable options
+const options = ["Rock", "Paper", "Scissors"];
 // User vs Computer
 // define variable user input *prompt?
 // define the variable that the computer uses
