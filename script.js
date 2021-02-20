017c5b54435e33156170bd552a154f8c87493bd9
TMats <takomats@hotmail.com>
2021-02-20 12:38:08 -0500

    Added Foundation

---
+++ script.js
@@ -0,0 +1,6 @@
+// define Rock, Paper, Scissors as viable options
+// User vs Computer
+// define variable user input *prompt?
+// define the variable that the computer uses
+// Use `alert()`, `confirm()`, and `prompt()` methods
+// last step, computers selection at random
