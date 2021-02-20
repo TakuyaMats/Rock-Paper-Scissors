e52338332a3d08ab4e0fd121595c3f092f7b7f0a
thebadams <bpeteradams@gmail.com>
2021-02-20 13:05:11 -0500

    Finished random computer logic

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 // define Rock, Paper, Scissors as viable options
 const options = ["Rock", "Paper", "Scissors"];
+var numOption
+var numChoice
+// Generate random option
+function randomNum() {
+    numOption = Math.floor(Math.random() * 3);
+    return numOption;
+};
+
+function randomChoice(){
+    randomNum()
+    computerChoice = options[numOption];
+    return computerChoice;
+};
 // User vs Computer
 // define variable user input *prompt?
 // define the variable that the computer uses
