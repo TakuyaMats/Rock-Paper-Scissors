9ca7ef9d0e046a109b8b78140d6c5ee94de8e609
thebadams <bpeteradams@gmail.com>
2021-02-20 20:23:39 -0500

    modified starting point for logic.js

--- script.js
+++ script.js
@@ -7,15 +7,27 @@
 
 
 // user choices
-var userChoice = prompt("Do you want to pick Rock, Paper, or Scissors");
 
-const options = ["R", "P", "S"];
-var computerChoice = randomChoice();
-var numOption
-var numChoice
+// Define variables
+const options = ["Rock", "Paper", "Scissors"];
+var isGameOver = false
+var userChoice = prompt("Do you want to pick Rock, Paper, or Scissors");
+var computerChoice = "Rock"
+var playerWins = 0;
+var computerWins = 0;
+
+
+
+
+
+
 
 
 // Generate random option
+
+var numOption
+var numChoice
+
 function randomNum() {
     numOption = Math.floor(Math.random() * 3);
     return numOption;
@@ -25,32 +37,3 @@ function randomChoice(){
     computerChoice = options[numOption];
     return computerChoice;
 };
-
-var playerWins=0;
-var computerWins=0;
-
-if (userChoice === "R" && computerChoice === "S"){
-    alert("You win!");
-    playerWins++
-} else if (userChoice === "R" && computerChoice === "P") {
-    alert("Computer wins!");
-    computerWins++
-} else if (userChoice === "P" && computerChoice === "R") {
-    alert("You win!")
-    playerWins++
-} else if (userChoice === "P" && computerChoice === "S") {
-    alert("Computer wins!")
-    computerWins++
-} else if (userChoice === "S" && computerChoice === "P") {
-    alert("You win!")
-    playerWins++
-} else if (userChoice === "S" && computerChoice === "R"){
-    alert("Computer wins!")
-    computerWins++
-}
-
-if (window.confirm("Do you want to replay the game?")) {
-    window.open("Refresh the page to try again!");
-  }
-
-var isGameOver = false
